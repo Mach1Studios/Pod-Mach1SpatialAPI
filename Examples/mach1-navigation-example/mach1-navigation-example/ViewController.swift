1184406ea0ad81933e6ca819487a1b70a3a7ae41
Dylan Marcus <dylan@mach1studios.com>
2021-03-19 12:33:29 -0600

    examples: minor fixes

--- Examples/mach1-navigation-example/mach1-navigation-example/ViewController.swift
+++ Examples/mach1-navigation-example/mach1-navigation-example/ViewController.swift
@@ -6,20 +6,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var Encoder: Encoder! = Encoder()
+    var encoder: Encoder! = Encoder()
     
     @IBAction func PlayButtonPressed(_ sender: Any) {
         print("Play pressed")
         
-        Encoder.activateAudioSession()
-        Encoder.playSpatialAudioCue(audioCue: "Trying to play that")
+        encoder.activateAudioSession()
+        encoder.playSpatialAudioCue(audioCue: "Trying to play that")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        playerArray.setupAudio(globalGain: 1.0)
+        encoder.setupAudio(globalGain: 1.0)
     }
 
 }
